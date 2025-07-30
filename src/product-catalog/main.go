4cb87b1464fa541a1c9f68245fe841741c23315b
Prithvich97 <113849850+Prithvich97@users.noreply.github.com>
2025-07-30 14:31:45 -0400

    ci/cd verfying:v13

--- src/product-catalog/main.go
+++ src/product-catalog/main.go
@@ -283,6 +283,8 @@ func (p *productCatalog) GetProduct(ctx context.Context, req *pb.GetProductReque
 }
 
 
+
+
 func (p *productCatalog) SearchProducts(ctx context.Context, req *pb.SearchProductsRequest) (*pb.SearchProductsResponse, error) {
 	span := trace.SpanFromContext(ctx)
 
